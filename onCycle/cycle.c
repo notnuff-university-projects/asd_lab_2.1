10e9783462f691e6a12559935882b00dd3f8ac60
notnuff <yaroshsasha960@gmail.com>
2023-03-11 20:44:07 +0200

    added cycle algorithm

---
+++ onCycle/cycle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+double ln(double x, int depth) {
+    double sum = 0, calc, res;
+    for (int i = 1; i <= depth; i++) {
+        calc = -x * (i - 1) / i;
+        res = (i != 1) ? res * calc : x;
+        sum += res;
+
+        printf("sum = %f res = %f i = %i\n", sum, res, i);
+    }
+    return sum;
+}
+
+int main() {
+    printf ("result = %f\n", ln(0.5, 5));
+    printf ("library result = %f", log(1.5));
+    return 0;
+}
