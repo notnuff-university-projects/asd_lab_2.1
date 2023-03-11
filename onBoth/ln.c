043a397759ef948806c649bd46e95c6c4ed3bbd2
notnuff <yaroshsasha960@gmail.com>
2023-03-11 20:45:55 +0200

    remade onBoth, added LOTS of improvement

--- onBoth/ln.c
+++ onBoth/ln.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
 #include <math.h>
 
+struct result {
+    double current;
+    double sum;
+};
+
+struct result serviceLn(double x, double curr, int i, int depth) {
+    double calc, local;
+    struct result res;
+
+    res.current = curr;
+    local = res.current;
+
+    printf("local = %f res.current = %f res.sum = %f i = %i\n", local, res.current, res.sum, i - 1);
+
+    if (i - 1 == depth) {
+        res.sum = local;
+        return res;
+    }
+
+    else {
+        calc = -x * (i - 1) / i;
+        res.current *= calc;
+        res = serviceLn(x, res.current, i + 1, depth);
+    }
+
+    res.sum += local;
+    printf("local = %f res.current = %f res.sum = %f i = %i\n", local, res.current, res.sum, i - 1);
+
+    return res;
+}
+
+double ln(double x, int i) {
+    return serviceLn(x, x, 2, i).sum;
+}
 
 int main() {
-    printf ("result = %f\n", ln(0.5, 18));
+    printf ("result = %f\n", ln(0.5, 5));
     printf ("library result = %f", log(1.5));
     return 0;
 }
